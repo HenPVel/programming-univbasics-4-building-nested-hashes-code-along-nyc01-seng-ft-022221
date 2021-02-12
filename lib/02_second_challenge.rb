cdb1319b708c8c8a46503b3daa2f8e648213b570
Henry Velasquez <hvelas93@gmail.com>
2021-02-12 03:06:24 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -5,15 +5,18 @@ def second_challenge
       patriarch:{}
       matriarch:{}
       hero:{}
-      
-      
-      }, 
+      hero_friends:[]
+
+
+      },
     :capulet => {
       #your key/value pairs here!
       patriach:{}
       matriarch:{}
       heroine:{}
-      }}
+      heroine_friends:[]
+      }
+    }
 
-  
+
 end
