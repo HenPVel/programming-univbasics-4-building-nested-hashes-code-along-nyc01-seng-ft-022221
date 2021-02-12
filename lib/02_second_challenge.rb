9f025edfdd257015e9c4298770925e1cfe9e3e7c
Henry Velasquez <hvelas93@gmail.com>
2021-02-12 03:05:00 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -2,9 +2,17 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      patriarch:{}
+      matriarch:{}
+      hero:{}
+      
+      
       }, 
     :capulet => {
       #your key/value pairs here!
+      patriach:{}
+      matriarch:{}
+      heroine:{}
       }}
 
   
